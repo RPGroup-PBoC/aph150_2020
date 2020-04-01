a2f7d3f434ca2dad2f49bae074121d2669a116c1
Griffin Chure <gchure@caltech.edu>
2020-04-01 13:59:14 -0700

    Formatted the license in the readme

--- fluora_fauna/gapminder_world_population_1800-2100/code/brazil_us_plot.py
+++ fluora_fauna/gapminder_world_population_1800-2100/code/brazil_us_plot.py
@@ -1,7 +1,7 @@
 """
 This script uses the Altair plotting library to generate a plot of the
-populations of Brazil and the US over the range of years in the original
-dataset. 
+populations of Brazil and the US over the range of year from present in 
+the original dataset. 
 """
 #%%
 import pandas as pd
@@ -9,15 +9,16 @@ import altair as alt
 
 # Load in the data. 
 data = pd.read_csv('../gapminder_world_population_1800-2100_tidy.csv')
+data['year_from_present'] = data['year'].values - 2020
 
 # Restrict to only Brazil and the US
 restricted = data[data['country'].isin(['Brazil', 'United States'])]
 
 # Generate the plot. 
 plot = alt.Chart(restricted).mark_line().encode(
-        x=alt.X('year:Q'),
+        x=alt.X('year_from_present:Q', axis={'title':'years from present (2020)'}),
         y=alt.Y('population:Q', scale={'type':'log'}),
         color='country:N'
 )
-plot
+plot.save('../media/brazil_us_population.png')
 # %%
